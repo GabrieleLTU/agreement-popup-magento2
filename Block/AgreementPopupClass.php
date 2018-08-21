57515b0121f39facf820fb2432b9aca612e45313
GabrieleLTU <g.valaikaite@gmail.com>
2018-08-21 09:34:55 +0300

    getInfo finction returned to AgreementPopupClass (and deleted from controller)

--- Block/AgreementPopupClass.php
+++ Block/AgreementPopupClass.php
@@ -7,20 +7,23 @@ use Visma\AgreementPopup\Controller\AgreementCookieController;
 class AgreementPopupClass extends \Magento\Framework\View\Element\Template
 {
     protected $_cookieController;
+    protected $_scopeConfig;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         AgreementCookieController $agreementCookieController,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         array $data = []
    )
    {
         $this->_cookieController = $agreementCookieController;
+       $this->_scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
    }
 
    public function getCookie()
    {
-       if(!isset($_COOKIE['agreement'])) {
+       if(!isset($_COOKIE['agreement'])) {//cookie name as const is in cookieHelper
 
            $this->_cookieController->setCookie("false");
        }
@@ -37,6 +40,14 @@ class AgreementPopupClass extends \Magento\Framework\View\Element\Template
 
     public function getConfigInfo()
     {
-        return $this->_cookieController->getConfigInfo();
+        $agreementText = $this->_scopeConfig->getValue('clientagreement/general/display_agreement_text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $agreementUrlText = $this->_scopeConfig->getValue('clientagreement/general/agreement_url_text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $agreementUrl = $this->_scopeConfig->getValue('clientagreement/general/agreement_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+        return array (
+            "agreement_text" => $agreementText,
+            "agreement_url_text" => $agreementUrlText,
+            "agreement_url" => $agreementUrl
+        );
     }
 }
--- Controller/AgreementCookieController.php
+++ Controller/AgreementCookieController.php
@@ -10,19 +10,16 @@ class AgreementCookieController extends Template
 {
     protected $_cookieManager;
     protected $_cookieHelper;
-    protected $_scopeConfig;
 
     public function __construct(
         Context $context,
         CookieHelper $cookieHelper,
         \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         array $data = []
     )
     {
         $this->_cookieManager = $cookieManager;
         $this->_cookieHelper = $cookieHelper;
-        $this->_scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
     }
 
@@ -34,18 +31,5 @@ class AgreementCookieController extends Template
     public function setCookie($value)
     {
         $this->_cookieHelper->setCookie($value);
-    }
-
-    public function getConfigInfo()
-    {
-        $agreementText = $this->_scopeConfig->getValue('clientagreement/general/display_agreement_text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $agreementUrlText = $this->_scopeConfig->getValue('clientagreement/general/agreement_url_text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $agreementUrl = $this->_scopeConfig->getValue('clientagreement/general/agreement_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-
-        return array (
-            "agreement_text" => $agreementText,
-            "agreement_url_text" => $agreementUrlText,
-            "agreement_url" => $agreementUrl
-        );
     }
 }
